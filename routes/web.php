7eb3da7327cfa279b5c8f8bd9e42717bd2bfdca1
DonMartinWorks <donmartinworks@gmail.com>
2024-10-17 18:57:32 -0300

    [LOGIN] - Creación de la vista admin login

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::get('/admin/login', function () {
+    return view('admin.auth.login');
+});
+
 require __DIR__.'/auth.php';
